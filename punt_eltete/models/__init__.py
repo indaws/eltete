5c567c81addf1f2cf484bf3dc8fffa04fb40617b
indaws <36474710+indaws@users.noreply.github.com>
2019-12-31 12:55:48 +0100

    Add files via upload

--- punt_eltete/models/__init__.py
+++ punt_eltete/models/__init__.py
@@ -7,3 +7,4 @@ from . import wizard
 from . import sale_order
 from . import stock_production_lot
 from . import product_pricelist
+from . import stock_picking
---
+++ punt_eltete/models/stock_picking.py
@@ -0,0 +1,22 @@
+﻿
+from odoo import fields, models, api
+
+
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+    
+    
+    
+    num_pallets = fields.Integer('Num pallets', compute="_get_num_pallets")
+    
+    @api.depends('move_lines')
+    def _get_num_pallets(self):
+    
+        for record in self:
+            num_pallets = 0
+            for line in record.move_lines:
+                num_pallets = num_pallets + int(line.product_uom_qty)
+            record.num_pallets = num_pallets
+    
