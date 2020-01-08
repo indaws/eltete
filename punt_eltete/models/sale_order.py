92fc0e878ac586cbbd8ca12b3212b39c39073522
indaws <36474710+indaws@users.noreply.github.com>
2020-01-08 17:21:15 +0100

    Add files via upload

--- punt_eltete/models/sale_order.py
+++ punt_eltete/models/sale_order.py
@@ -70,7 +70,8 @@ class SaleOrderLine(models.Model):
     
     @api.onchange('oferta_id', 'num_pallets', 'und_user', 'kilos_user' )
     def _onchange_oferta_cantidad(self):
-        self.price_unit = self.importe / self.num_pallets
+        if self.num_pallets > 0:
+            self.price_unit = self.importe / self.num_pallets
         self.product_uom_qty = self.num_pallets
     
     @api.depends('oferta_id', 'num_pallets', 'und_user', 'kilos_user')
