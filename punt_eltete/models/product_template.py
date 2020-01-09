ff75584c48c77b2ba441ae675de3ffc86503b1b4
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-01-09 13:36:08 +0100

    Update product_template.py

--- punt_eltete/models/product_template.py
+++ punt_eltete/models/product_template.py
@@ -339,7 +339,7 @@ class ProductCategory(models.Model):
             titulo = titulo + "Fino Kraft Blanco Brillo"
         elif papel == '20':
             titulo = titulo + "Gordo Kraft Marrón"
-        """
+        
         titulo = titulo + " - "
         if fsc_tipo == '0':
             #titulo = titulo + "NINGUNO"
@@ -348,14 +348,14 @@ class ProductCategory(models.Model):
         elif fsc_tipo == '2':
             titulo = titulo + "FSC MIX CREDIT"
         elif fsc_tipo == '3':
-            titulo = titulo + "FSC MIX " + fsc_valor + " %"
+            titulo = titulo + "FSC MIX " + str(fsc_valor) + " %"
         elif fsc_tipo == '4':
             titulo = titulo + "FSC RECYCLED CREDIT"
         elif fsc_tipo == '5':
-            titulo = titulo + "FSC RECYCLED " + fsc_valor + " %"
+            titulo = titulo + "FSC RECYCLED " + srt(fsc_valor) + " %"
         elif fsc_tipo == '6':
             titulo = titulo + "FSC CONTROLLED WOOD"
-      
+      """
         product_name = "PAPEL - " + titulo
         referencia_id = self.env['product.referencia'].create({'name': product_name, 
                                                           'titulo': titulo, 
