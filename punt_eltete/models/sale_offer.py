a148f8db8f116c6dae51e526ed0929f77cdedfa1
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-01-21 12:31:28 +0100

    Add files via upload

--- punt_eltete/models/sale_offer.py
+++ punt_eltete/models/sale_offer.py
@@ -190,10 +190,8 @@ class sale_referencia_cliente(models.Model):
                         #Compacto
                         elif paletizado == 1:
                             und_paquete = 25
-                            if record.referencia_id.grosor_2 >= 6:
+                            if record.referencia_id.grosor_2 >= 5:
                                 und_paquete = 10
-                            elif record.referencia_id.grosor_2 >= 5:
-                                und_paquete = 15
                             elif record.referencia_id.grosor_2 >= 4:
                                 und_paquete = 20
                             pesoPaquete = und_paquete * pesoUnidad
