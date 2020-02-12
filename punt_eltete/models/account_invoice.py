67db04e803f0c80a4f2066c873de1e8a9a1147b7
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-02-12 09:22:07 +0100

    Update account_invoice.py

--- punt_eltete/models/account_invoice.py
+++ punt_eltete/models/account_invoice.py
@@ -33,7 +33,7 @@ class AccountInvoice(models.Model):
                     if line.product_id.type == 'product':
                         num_pallets = num_pallets + line.num_pallets
                 
-                if line.importe_incorrecto = True:
+                if line.importe_incorrecto == True:
                     incorrecto = True
                 calculado = calculado + line.importe_calculado
                 
