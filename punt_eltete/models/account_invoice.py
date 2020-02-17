66bdd732fa07cff1cda88aa1993d50b77f0a708c
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-02-17 11:51:40 +0100

    Update account_invoice.py

--- punt_eltete/models/account_invoice.py
+++ punt_eltete/models/account_invoice.py
@@ -10,7 +10,7 @@ class AccountInvoice(models.Model):
     num_pallets = fields.Integer('Num pallets', readonly = True, compute = "_get_num_pallets")
     peso_neto = fields.Integer('Peso Neto', readonly = True, compute = "_get_num_pallets")
     peso_cantonera = fields.Integer('Peso Cantoneras', readonly = True, compute = "_get_num_pallets")
-    peso_slisheet = fields.Integer('Peso Slip Sheets', readonly = True, compute = "_get_num_pallets")
+    peso_slipsheet = fields.Integer('Peso Slip Sheets', readonly = True, compute = "_get_num_pallets")
     
     importe_sin_descuento = fields.Float('Importe Sin Descuento', digits = (10, 2), compute="_get_valores_descuento")
     importe_descuento = fields.Float('Importe Dto PP', digits = (10, 2), compute="_get_valores_descuento")
