7f60ffad2af0114f18405340e0a37bbd04b108dd
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-03-09 13:38:44 +0100

    Update purchase_order.py

--- punt_eltete/models/purchase_order.py
+++ punt_eltete/models/purchase_order.py
@@ -110,18 +110,20 @@ class PurchaseOrderLine(models.Model):
             if record.product_id:
                 if record.product_id.categ_id.is_mprima_cola == True or record.product_id.categ_id.is_mprima_papel == True:
                     cantidad_num = record.kg_pedidos
+                    importe = cantidad_num * record.precio_num
                     cantidad = str(cantidad_num) + "<br/>" + "Kg"
                     precio = str(record.precio_num) + "<br/>" + "€/kg"
                 elif record.product_id.categ_id.is_formato == True or record.product_id.categ_id.is_bobina == True:
                     cantidad_num = record.peso_neto_pallet * record.num_pallets
+                    importe = cantidad_num * record.precio_num
                     cantidad = str(cantidad_num) + "<br/>" + "Kg"
                     precio = str(record.precio_num) + "<br/>" + "€/kg"
                 else:
                     cantidad_num = record.und_pallet * record.num_pallets
+                    importe = cantidad_num * record.precio_num
                     cantidad = str(cantidad_num) + "<br/>" + "Unidades"
                     precio = str(record.precio_num) + "<br/>" + "€/unidad"
                     
-            importe = float(cantidad_num) * record.precio_num
             record.importe_pedido = importe
             record.cantidad_pedida = cantidad
             record.precio = precio
