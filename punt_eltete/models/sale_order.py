e138fbba375dd46391059e48b6240ed5c8dd704c
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-03-12 17:01:20 +0100

    Update sale_order.py

--- punt_eltete/models/sale_order.py
+++ punt_eltete/models/sale_order.py
@@ -706,10 +706,27 @@ class SaleOrder(models.Model):
                 if line.codigo_cliente and len(line.codigo_cliente) > 0:
                     haycodigo = True
             
-            peso_neto_mojado = (1 + (peso_neto / 200000)) * peso_neto
+            divisor = 150000
+            peso_neto_mojado = (1 + (peso_neto / divisor)) * peso_neto
             peso_neto_mojado = int(peso_neto_mojado / 5) * 5
-            peso_bruto_mojado = (1 + (peso_bruto / 200000)) * peso_bruto
+            peso_bruto_mojado = (1 + (peso_bruto / divisor)) * peso_bruto
             peso_bruto_mojado = int(peso_bruto_mojado / 5) * 5
+            
+            if peso_bruto < 24000:
+                while peso_bruto_mojado > 24000:
+                    divisor = divisor + 10000
+                    peso_neto_mojado = (1 + (peso_neto / divisor)) * peso_neto
+                    peso_neto_mojado = int(peso_neto_mojado / 5) * 5
+                    peso_bruto_mojado = (1 + (peso_bruto / divisor)) * peso_bruto
+                    peso_bruto_mojado = int(peso_bruto_mojado / 5) * 5
+                    
+                if peso_neto_mojado < peso_neto:
+                    peso_neto_mojado = peso_neto
+                if peso_bruto_mojado < peso_bruto:
+                    peso_bruto_mojado = peso_bruto 
+            else:
+                peso_neto_mojado = peso_neto
+                peso_bruto_mojado = peso_bruto
             
             if peso_neto > 0:
                 eton = eton / peso_neto
