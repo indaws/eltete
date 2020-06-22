5cb094c554b888518ef149012d142ca08c021cfe
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-06-22 12:09:57 +0200

    Update sale_order.py

--- punt_eltete/models/sale_order.py
+++ punt_eltete/models/sale_order.py
@@ -598,15 +598,16 @@ class SaleOrderLine(models.Model):
                 velocidad = velocidad - 20
                 
             dir_qr_orden = "http://bemecopack.es/jseb/qr_orden.php?"
-            dir_qr_orden = disr_qr_orden + "orden=" + orden_fabricacion
-            dir_qr_orden = disr_qr_orden + "&prod=1"
-            dir_qr_orden = disr_qr_orden + "&ala1=" + str(ala_1)
-            dir_qr_orden = disr_qr_orden + "&ala2=" + str(ala_2)
-            dir_qr_orden = disr_qr_orden + "&gros=" + str(grosor)
-            dir_qr_orden = disr_qr_orden + "&long1=" + str(longitud)
-            dir_qr_orden = disr_qr_orden + "&peso1=" + str(peso_unidad)
-            dir_qr_orden = disr_qr_orden + "&palini=" + str(record.lotes_inicio)
-            dir_qr_orden = disr_qr_orden + "&npal=" + str(record.lotes_fabricar)
+            dir_qr_orden = dir_qr_orden + "orden=" + orden_fabricacion
+            dir_qr_orden = dir_qr_orden + "&prod=1"
+            dir_qr_orden = dir_qr_orden + "&ala1=" + str(ala_1)
+            dir_qr_orden = dir_qr_orden + "&ala2=" + str(ala_2)
+            dir_qr_orden = dir_qr_orden + "&gros=" + str(grosor)
+            dir_qr_orden = dir_qr_orden + "&long1=" + str(longitud)
+            dir_qr_orden = dir_qr_orden + "&peso1=" + str(peso_unidad)
+            dir_qr_orden = dir_qr_orden + "&palini=" + str(record.lotes_inicio)
+            dir_qr_orden = dir_qr_orden + "&npal=" + str(record.lotes_fabricar)
+            record.dir_qr_orden = dir_qr_orden
             
             record.op_cantonera_maquina = maquina
             record.op_superficie_color = superficie_color
