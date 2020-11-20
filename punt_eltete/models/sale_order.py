80e25e3fd307f4aabb54c0c7abe1acdd7ca1185b
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2020-11-20 11:45:08 +0100

    Update sale_order.py

--- punt_eltete/models/sale_order.py
+++ punt_eltete/models/sale_order.py
@@ -489,25 +489,23 @@ class SaleOrderLine(models.Model):
             superficie_ancho = "" 
             interior_ancho = ""
             interior_gramaje = ""
-            j_gram = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.j_gram
-            
+
             if record.oferta_id.attribute_id.is_cantonera == True:
                 if record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho_superficie:
                     superficie_ancho = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho_superficie
                 if record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho_interior:
                     interior_ancho = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho_interior 
                 
-                aux2 = j_gram + 100
-                interior_gramaje = str(j_gram) + " - " + str(aux2)
+                aux2 = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.j_gram + 100
+                interior_gramaje = str(record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.j_gram) + " - " + str(aux2)
                     
             elif record.oferta_id.attribute_id.is_slipsheet == True:
                 interior_ancho = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho
                 interior_ancho = interior_ancho + record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ala_1
                 interior_ancho = interior_ancho + record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ala_2 
                 superficie_ancho = interior_ancho + 30
-                
-                aux2 = j_gram - 50
-                interior_gramaje = str(aux2) + " - " + str(j_gram)
+
+                interior_gramaje = record.oferta_id.attribute_id.gramaje + " g"
                 
             elif record.oferta_id.attribute_id.is_solidboard == True:
                 interior_ancho = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ancho
@@ -515,8 +513,7 @@ class SaleOrderLine(models.Model):
                 interior_ancho = interior_ancho + record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ala_2
                 superficie_ancho = interior_ancho + 30
                 
-                aux2 = j_gram - 50
-                interior_gramaje = str(aux2) + " - " + str(j_gram)
+                interior_gramaje = record.oferta_id.attribute_id.gramaje + " g"
             
             ala_1 = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ala_1
             ala_2 = record.oferta_id.attribute_id.referencia_cliente_id.referencia_id.ala_2
