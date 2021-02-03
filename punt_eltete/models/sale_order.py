8cdca06009913cf4ae6ae3011d2e282825f106fb
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2021-02-03 16:33:41 +0100

    Update sale_order.py

--- punt_eltete/models/sale_order.py
+++ punt_eltete/models/sale_order.py
@@ -882,7 +882,7 @@ class SaleOrderLine(models.Model):
                 if len(record.fila_vinculada_id.lot_ids) < record.fila_vinculada_id.num_pallets:
                     #und_pallet = record.fila_vinculada_id.und_pallet * record.fila_vinculada_id.num_pallets
                     if record.fila_vinculada_id.oferta_id:
-                        und_pallet = record.fila_vinculada_id.oferta_id.unidades
+                        und_pallet = record.fila_vinculada_id.oferta_id.unidades * record.fila_vinculada_id.num_pallets
                 else:
                     und_pallet = record.fila_vinculada_id.und_lotes
             else:
