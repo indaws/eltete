e221dea477ce17d1fa421ffe7c8f4ada11462be4
BemecoPack <59054267+BemecoPack@users.noreply.github.com>
2021-04-29 13:33:29 +0200

    Update res_partner.py

--- punt_eltete/models/res_partner.py
+++ punt_eltete/models/res_partner.py
@@ -11,6 +11,18 @@ class ResPartner(models.Model):
     ice = fields.Char('ICE')
     num_bailen = fields.Integer('Num Bailén')
     representante_legal = fields.Html('Representante Legal')
+    TIPO_CLIENTE_SEL = [('0','Sin Tipo'),
+                        ('1','Especial 660'),
+                        ('2','Camión Completo 700'),
+                        ('3','Agricultura 740'),
+                        ('4','Medio Camión 740'),
+                        ('5','Distribuidor Grande 780'),
+                        ('6','Distribuidor Mediano 820'),
+                        ('7','Distribuidor Pequeño 860'),
+                        ('7','Cliente Final Grande 820'), 
+                        ('8','Cliente Final Mediano 900'), 
+                        ('9','Cliente Final Pequeño 980'),
+                        ]
     
     
     prod_comment_ids = fields.One2many('partner.product.comments', 'partner_id', string="Observaciones productos")
